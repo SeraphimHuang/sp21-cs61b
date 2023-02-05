a9a24abf86237b8df913cfc622e579195a29afb4
ziyehuang <ziyehuang69@stu.pku.edu.cn>
2023-02-05 10:13:53 +0800

    finished homework1

--- lab1/LookingForGreatest.java
+++ lab1/LookingForGreatest.java
@@ -11,6 +11,6 @@ public class LookingForGreatest {
         }
     public static void main(String[] args) {
         int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6};
-        System.out.print(forMax(numbers));
+        System.out.println(forMax(numbers));
     }
 }
